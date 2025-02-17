0dfcf9e739ad36bb0a29d18cb73055b90e97ea60
Ashutosh Mane <53065203+AshutoshTMane@users.noreply.github.com>
2025-02-16 21:47:16 -0800

    Update main.c without additional code

--- CMPE146_Lab2E1_DriverLib/main.c
+++ CMPE146_Lab2E1_DriverLib/main.c
@@ -10,17 +10,10 @@
 int main(void)
 {
 
-    const uint32_t port_module = 0x40004C00;
-    const uint8_t PORT1_IN = 0x000;
-    const uint8_t PORT2_IN = 0x001;
-    const uint8_t PORT1_OUT = 0x002;
-    const uint8_t PORT2_OUT = 0x03;
-
     uint8_t button, previousVal;
     /* Halting the Watchdog */
     MAP_WDT_A_holdTimer();
 
-    /* Configuring P1.0 as output */
     MAP_GPIO_setAsOutputPin(GPIO_PORT_P2, GPIO_PIN2);
     MAP_GPIO_setOutputLowOnPin(GPIO_PORT_P2, GPIO_PIN2);
 
