23be727b458c91b5f3945f1a456f3346e37aab6a
Ashutosh Mane <53065203+AshutoshTMane@users.noreply.github.com>
2025-02-23 14:46:11 -0800

    Update main.c

--- CMPE146_Lab2E1_DriverLib3/main.c
+++ CMPE146_Lab2E1_DriverLib3/main.c
@@ -19,10 +19,12 @@ int main(void)
 
 
     uint8_t button, previousVal;
+
+    uint8_t* button_alias = (uint8_t*) ((port_module + port1_in - port_addr) * 32 + (1 * 4) + alias_addr);
+    uint8_t* led_alias = (uint8_t*) ((port_module + port2_out - port_addr) * 32  + (4 * 2) + alias_addr);
+
     /* Halting the Watchdog */
     MAP_WDT_A_holdTimer();
-    uint8_t *LED_ADDR = (uint8_t*)(port_module + port2_out);
-
 
     /* Configuring P1.0 as output */
     MAP_GPIO_setAsOutputPin(GPIO_PORT_P2, GPIO_PIN2);
@@ -31,15 +33,20 @@ int main(void)
     MAP_GPIO_setAsInputPinWithPullUpResistor(GPIO_PORT_P1, GPIO_PIN1);
     button = MAP_GPIO_getInputPinValue(GPIO_PORT_P1, GPIO_PIN1);
 
+    button = *button_alias;
+
     previousVal = button;
 
-    while(1){
-        button = *(uint8_t*) (port_module + port1_in);
+    //config push button as input P1.1
 
-        if(button != previousVal && (previousVal & 0x2) != 0x0){
-            *LED_ADDR ^= 0x4;
+    while(1){
+        button = *button_alias;
+
+        if(button != previousVal && (previousVal) != 0x0){
+            *led_alias ^= 0x1;
         }
         previousVal = button;
     }
+
 
 }
